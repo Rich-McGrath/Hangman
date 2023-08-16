99f073f70dd2aa9511d521d56b582a63fa07e1f1
Rich <richmcgrath@gmail.com>
2023-08-16 10:03:25 -0500

    Update hangman.py
    Fixed bug with End of Game Detection

--- hangman.py
+++ hangman.py
@@ -88,9 +88,12 @@ while not end_of_game:
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
-    if letter != guess:
-      print(stages[lives]) 
-      lives = lives - 1
+    if guess not in chosen_word:
+      lives -= 1
+      if lives == 0:
+        end_of_game = True
+        print("You lose.")
+      
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -98,6 +101,7 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-    elif lives == 0:
-        end_of_game = True
-        print("You lose")
+         
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
