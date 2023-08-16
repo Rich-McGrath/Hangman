5a93357d230053223e1ca063790279241881bb27
Rich <richmcgrath@gmail.com>
2023-08-16 10:45:12 -0500

    Update hangman.py
    Added clear screen after each guess.

--- hangman.py
+++ hangman.py
@@ -21,6 +21,8 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    clear()
+    
     if guess in display:
       print(f"You've already guessed {guess}")
       
